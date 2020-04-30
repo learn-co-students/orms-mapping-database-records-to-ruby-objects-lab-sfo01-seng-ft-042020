b84890527a5349ce515b5e266f3c0c4181d5d5ab
Don Mallory <dmm333@gmail.com>
2020-04-29 20:01:58 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,19 +2,56 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    stud = Student.new
+    stud.id, stud.name, stud.grade = row
+    stud
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    find_between_grades(0, 12)
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ?
+    SQL
+    new_from_db(DB[:conn].execute(sql, name)[0])
+  end
+
+  def self.all_students_in_grade_9
+    find_between_grades(9, 9)
+  end
+
+  def self.students_below_12th_grade
+    find_between_grades(0, 11)
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    find_between_grades(10, 10, x)
+  end
+
+  def self.first_student_in_grade_10
+    find_between_grades(10, 10, 1).first
+  end
+
+  def self.all_students_in_grade_X(x)
+    find_between_grades(x, x)
   end
   
+  def self.find_between_grades(low, high, limit = nil) 
+    params = [low, high, limit].compact
+    if limit.nil?
+      sql = <<-SQL
+      SELECT * FROM students WHERE grade BETWEEN ? AND ? 
+      SQL
+    else
+      sql = <<-SQL
+      SELECT * FROM students WHERE grade BETWEEN ? AND ? LIMIT ? 
+      SQL
+    end
+    DB[:conn].execute(sql, params).map {|row| new_from_db(row)}
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
